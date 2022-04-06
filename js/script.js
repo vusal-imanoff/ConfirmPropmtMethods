c215970a0d47a0735359974bbce0ef0d9c82d9ae
vusal-imanoff <vusalmi@code.edu.az>
2022-04-06 13:31:59 +0400

    done

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+// function Click(){
+//     let text = "press ok button";
+//     if(confirm(text)==true){
+
+//         document.body.style.backgroundColor="black";
+//     }
+//     else{
+//         document.body.style.backgroundColor="white";
+//     }
+// }
+
+
+
+
+let age=prompt("enter your age:")
+if(age<18){
+    alert("your age than small 18");
+}
+else{
+    alert("you are older")
+}
